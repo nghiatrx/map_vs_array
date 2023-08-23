dcf9cc9910ff7b6d5d42fdda73ceeb04bfb29850
trong nghia <nghiadt@nhanlucsieuviet.com>
2023-08-23 15:15:55 +0700

    update

--- main_test.go
+++ main_test.go
@@ -30,6 +30,24 @@ func Benchmark1M(b *testing.B) {
 	})
 }
 
+func Benchmark50M(b *testing.B) {
+	num := 50000000
+	myMap := make(map[int]int, num)
+	myArray := make([]int, num)
+
+	for i := 0; i < num; i++ {
+		myMap[i] = 1
+		myArray[i] = i
+	}
+
+	b.Run("map", func(b *testing.B) {
+		benchMap(&myMap, num-1)
+	})
+	b.Run("array", func(b *testing.B) {
+		benchArray(&myArray, num-1)
+	})
+}
+
 func Benchmark100M(b *testing.B) {
 	num := 100000000
 	myMap := make(map[int]int, num)
